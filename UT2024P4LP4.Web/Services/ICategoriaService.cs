997f0fa10faabf20b1f7eb2e688fdf3b15731e63
richely <richely@RICHELY17>
2024-11-01 23:21:40 -0400

    Agregar archivos de proyecto.

---
+++ UT2024P4LP4.Web/Services/ICategoriaService.cs
@@ -0,0 +1,12 @@
+﻿using UT2024P4LP4.Web.Data.Dtos;
+namespace UT2024P4LP4.Web.Services
+{
+    public interface ICategoriaService
+    {
+        Task<Result> Create(CategoriaRequest categoria);
+        Task<Result> Delete(int Id);
+        Task<ResultList<CategoriaDto>> GetAll(string filtro = "");
+        Task<Result> Update(CategoriaRequest categoria);
+    }
+}
+  
